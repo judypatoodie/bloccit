2296fb3b38344175db0aa034802752e8dc171379
Judy Huynh <jhuynh@mossbeachhomes.com>
2019-02-25 15:26:47 -0800

    completed routing controllers and the view

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -2,23 +2,22 @@ const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
 const marcoBase = "http://localhost:3000/marco";
+const aboutBase = "http://localhost:3000/about";
 
 describe("routes : static", () => {
 
   describe("GET /", () => {
 
-    it("should return status code 200", (done) => {
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
 
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
+        expect(body).toContain("Welcome to Bloccit");
 
         done();
       });
     });
-  });
 
-
-  describe("GET /", () => {
       it("should return status code 200", (done) => {
         request.get(marcoBase, (err, res, body) => {
           expect(res.statusCode).toBe(200);
@@ -32,7 +31,11 @@ describe("routes : static", () => {
           done();
         });
       });
+      it("response body should contain string 'About Us'", (done) => {
+        request.get(aboutBase, (err, res, body) => {
+          expect(body).toContain('About Us');
+          done();
+        });
+      });
     });
-
-
 });
